348927e94de2980888a2f6a95cfb81f7e1bb04c5
소원 박 <s2018s29@e-mirim.hs.kr>
2020-10-10 21:01:49 +0900

    초기 커밋.

---
+++ LambdaStudy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaStudy
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Product> products = new List<Product>()
+            {
+                new Product(){Name="사과꽃", Price=100}
+                ,new Product(){Name="안개꽃",Price=8000}
+                ,new Product(){Name="장미꽃", Price=500}
+                ,new Product(){Name="국화꽃", Price=343000}
+                ,new Product(){Name="어쩌구",Price=140}
+            };//new Product
+
+            //정렬
+            //Sort() 메서드의 매개변수로 람다를 지정합니다
+            products.Sort((a, b) =>
+            {
+                return a.Price.CompareTo(b.Price);
+            });
+
+            //아래처럼 더 간단히 할 수도 있음
+            products.Sort((a, b) => a.Price.CompareTo(b.Price));
+
+            //출력
+            foreach (var item in products)
+            {
+                Console.WriteLine(item.Name + ":" + item.Price);
+            }
+
+        }
+    }//Main()
+}//class
